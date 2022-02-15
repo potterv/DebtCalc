a5ec503defc4bcea985f6b3942931093ec168697
Владимир Гончар <gonchar@092.pfr.ru>
2022-02-15 12:43:37 +0300

    uppdate algoritm araund

--- src/main/java/CalcDebt.java
+++ src/main/java/CalcDebt.java
@@ -1,8 +1,33 @@
 import calculate.Calc;
+import inteface.InDataCalc;
 
 public class CalcDebt {
     public static void main(String[] args) {
         Calc calc =new Calc();
+        InDataCalc inDataCalc = new InDataCalc();
+        boolean count = true;
+        while (count) {
+            try {
+                calc.setSumm(inDataCalc.getSumm());
+                calc.setColDaysInMonth(inDataCalc.getColDaysInMonth());
+                calc.setColDasInMonthPol(inDataCalc.getColDasInMonthPol());
+                calc.setColPolMonth(inDataCalc.getColPolMonth());
 
+            } catch (NumberFormatException e) {
+                e.printStackTrace();
+                System.out.println("posledniy vvedennyi parametr ne korreknyi");
+            } finally {
+
+                System.out.print(" REZULTAT : " + calc.resultSumm());
+                System.out.println("");
+                if (!inDataCalc.isCount()){
+                    break;
+                }
+            }
+
+
+
+
+        }
     }
 }
